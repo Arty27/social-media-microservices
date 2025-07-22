d009e39d8eff2a2d0c04438d7a33b854fa88a9b7
ec24186 <vinayn.1999@gmail.com>
2025-07-22 13:38:13 +0100

    added divider to logger file

--- api-gateway/src/utils/logger.ts
+++ api-gateway/src/utils/logger.ts
@@ -4,7 +4,7 @@ const { timestamp, errors, splat, json, combine, colorize, simple } =
   winston.format;
 const { File, Console } = winston.transports;
 
-export const logger: Logger = winston.createLogger({
+export const baseLogger: Logger = winston.createLogger({
   level: process.env.NODE_ENV === "production" ? "info" : "debug",
   format: combine(timestamp(), errors({ stack: true }), splat(), json()),
   defaultMeta: { service: "api-gateway" },
@@ -15,4 +15,13 @@ export const logger: Logger = winston.createLogger({
     new File({ filename: "error.log", level: "error" }),
     new File({ filename: "combined.log" }),
   ],
+});
+
+// Adding custom divider to the logs to differentiate between requests
+export const logger: Logger & {
+  divider: (char?: string, length?: number) => void;
+} = Object.assign(baseLogger, {
+  divider: (char = "-", length = 90): void => {
+    console.log(char.repeat(length));
+  },
 });
