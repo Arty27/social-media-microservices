8f7c5095bea6405815760016b8b1cfcaee89d84a
ec24186 <vinayn.1999@gmail.com>
2025-07-24 22:02:17 +0100

    media service files setup

---
+++ media-service/src/utils/logger.ts
@@ -0,0 +1,27 @@
+import winston, { Logger } from "winston";
+
+const { timestamp, errors, splat, json, combine, colorize, simple } =
+  winston.format;
+const { File, Console } = winston.transports;
+
+export const baseLogger: Logger = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: combine(timestamp(), errors({ stack: true }), splat(), json()),
+  defaultMeta: { service: "Media Service" },
+  transports: [
+    new Console({
+      format: combine(colorize(), simple()),
+    }),
+    new File({ filename: "error.log", level: "error" }),
+    new File({ filename: "combined.log" }),
+  ],
+});
+
+// Adding custom divider to the logs to differentiate between requests
+export const logger: Logger & {
+  divider: (char?: string, length?: number) => void;
+} = Object.assign(baseLogger, {
+  divider: (char = "-", length = 90): void => {
+    console.log(char.repeat(length));
+  },
+});
