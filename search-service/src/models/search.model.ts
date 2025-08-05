d2afa8a16dbfc2c35ae948df3755bfb3a2733b58
ec24186 <vinayn.1999@gmail.com>
2025-08-05 23:50:04 +0100

    Search Model created

---
+++ search-service/src/models/search.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Model, model, Schema } from "mongoose";
+
+export interface ISearch {
+  postId: string;
+  userId: string;
+  content: string;
+  createdAt?: Date;
+}
+
+export interface ISearchDocument extends Document, ISearch {}
+
+const searchSchema = new Schema<ISearchDocument>(
+  {
+    postId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+searchSchema.index({ content: "text" });
+searchSchema.index({ createdAt: -1 });
+
+export const Search: Model<ISearchDocument> = mongoose.model(
+  "Search",
+  searchSchema
+);
